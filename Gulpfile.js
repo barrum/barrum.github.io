658d408d1a2b655a9523ec2d584e2e61d73ba899
Dmitry O <fcinterna@yandex.ru>
2021-07-17 11:13:42 +0300

    build - > docs

--- Gulpfile.js
+++ Gulpfile.js
@@ -15,10 +15,10 @@ const browserSync = require('browser-sync').create();
 
 
 // =============================================================================
-// УДАЛЯЕТ ПАПКУ BUILD
+// УДАЛЯЕТ ПАПКУ docs
 // =============================================================================
 function clean() {
-  return del(['build']);
+  return del(['docs']);
 }
 
 // =============================================================================
@@ -31,7 +31,7 @@ function html() {
       suffix: ' -->',
       basepath: '_src/html/inc'
     }))
-    .pipe(dest('build'))
+    .pipe(dest('docs'))
     .pipe(browserSync.stream());
 }
 
@@ -48,18 +48,18 @@ function styles() {
       cascade: false
     }))
     .pipe(sourcemaps.write('./'))
-    .pipe(dest('build/css'))
+    .pipe(dest('docs/css'))
     .pipe(browserSync.stream());
 }
 
 // =============================================================================
-// КОПИРУЕТ ФАЙЛЫ И ПАПКИ ИЗ SRC В BUILD
+// КОПИРУЕТ ФАЙЛЫ И ПАПКИ ИЗ SRC В docs
 // =============================================================================
 function copyFiles() {
   return src(['_src/img/**', '_src/fonts/**'], {
       'base': '_src'
     }) // base треб. для коректного переноса файлов и папок (для fonts)
-    .pipe(dest('build'));
+    .pipe(dest('docs'));
 }
 
 // =============================================================================
@@ -68,23 +68,23 @@ function copyFiles() {
 function watching() {
   browserSync.init({
     server: {
-      baseDir: './build' // сервер запускается в этой директории
+      baseDir: './docs' // сервер запускается в этой директории
     },
     // browser: ["chrome"]
   });
   watch(['_src/less/**/*.less', /*'!_src/less/components/**'*/], styles); // следит за стилями
   watch('_src/html/**/*.html', html); // следит за html
   watch(['_src/img/**', '_src/fonts/**'], copyFiles); // следит за файлами
-  watch('build/*.html').on('change', browserSync.reload); // перегружает браузер, если файлы html в build изменились
+  watch('docs/*.html').on('change', browserSync.reload); // перегружает браузер, если файлы html в docs изменились
 }
 
 // =============================================================================
 // IMG -> BASE64
 // =============================================================================
 function base64() {
-  return src('build/index.html')
+  return src('docs/index.html')
     .pipe(img64())
-    .pipe(dest('build/base64'));
+    .pipe(dest('docs/base64'));
 }
 
 
